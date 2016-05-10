090261ebebc2b92fa82826c7fd0743e878f25011
StuddedExpandable <studdedexpandable@gmail.com>
2016-05-10 09:03:11 +0100

    Redone version, with help from NinjaStix

--- src/the/vox/mc/themasteredpanda/vlist/vlist.java
+++ src/the/vox/mc/themasteredpanda/vlist/vlist.java
@@ -3,7 +3,10 @@ package the.vox.mc.themasteredpanda.vlist;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -25,35 +28,96 @@ import com.google.common.base.Joiner;
 
 import net.milkbowl.vault.chat.Chat;
 
-public class vlist extends JavaPlugin implements Listener{
+//NOTE: By convention class names should start with an upper case letter
+public class vlist extends JavaPlugin implements Listener {
 
-	public ArrayList<String> staff = new ArrayList<String>();
-	Chat chat = null;
-	User user = null;
-	IEssentials ess = null;
-	public File configf;
-	public YamlConfiguration config;
+	/*
+	  tip: Since version 7 java has limited type inference for generic types
+	  Also always try to program against an interface rather than the implementation
+	  in this case if you don't functionality specific to the ArrayList use List
+	  this make it easy to change the underlying type if you ever need/want to.
+	  i.e. from ArrayList to LinkedList. Google for more info.
+	 */
+	//private List<String> staff = new ArrayList<>();
 
-	
-	public boolean hasNickName(Player p) {
-		return ((ess.getUser(p).getNickname() != null) && (!ess.getUser(p).getNickname().isEmpty()));
-	}
-	
+	//public ArrayList<String> staff = new ArrayList<String>();
+
+	/*
+	  BUT don't use a List for this xD use a Map
+	  I've also opted to define this in the onEnable() method.
+	 */
+	private Map<UUID, String> staff = null;
+
+	private Chat chat = null;
+	//private User user = null; //not needed
+	private IEssentials ess = null;
+
+	//I always consider it good practice to initialize all my instance variables
+	//to a default value when I declare them.
+	private File configf = null;
+	private YamlConfiguration config = null;
+	//private File configf;
+	//private YamlConfiguration config;
+
+	/*
+	  Also note I changed the access to private for all of the above instance variables.
+	  Since you only need access to these variables from within this class it doesn't make
+	  a huge difference, but its good practice to always follow anyway.
+	  Use getter and setter methods to provide access to any variables you need access to outside of the
+	  class they are declared in, this allows you to control exactly what users of your code, can and can
+	  not do with the variable, and also allows you to do additional conversions, etc. if need be. Its also
+	  just following the principals of OOP. Google Encapsulation. Keep your implementation details private.
+	 */
+
+	/*
+	  Added @Override annotation to onEnable() and onDisable() this makes it clear
+	  we are overriding these methods from anoter class.
+	 */
+
+	@Override
 	public void onEnable() {
 		createConfig();
 		setupChat();
 		ess = (IEssentials)getServer().getPluginManager().getPlugin("Essentials");
 		Bukkit.getServer().getPluginManager().registerEvents(this, this);
 		Bukkit.getLogger().info("[VList] is now enabled!");
+
+		staff = new HashMap<>();
+
+		//NOTE: You are never registering your list command. Make sure its in your plugin.yml and then:
+		getCommand("list").setExecutor(this);
 	}
+
+	@Override
 	public void onDisable() {
 		Bukkit.getLogger().info("[Vlist] is now disabled.....cya!");
 	}
-	
-	public void createConfig() {
+
+	/*
+	  I have changed the access to all your below methods to private. If you were to need
+	  access to one of the below methods from another class in your project, declare it as protected.
+	  Only declare the methods as public if you intend user to access that method from outside your code.
+	 */
+
+	private boolean hasNickName(Player p) {
+		String nickname = getNickname(p);
+		return (nickname != null) && (!nickname.isEmpty());
+	}
+
+	//the below two methods are for convenience
+	private String getNickname(Player player) {
+		return ess.getUser(player).getNickname();
+	}
+
+	private String getPrefix(Player player) {
+		return chat.getPlayerPrefix(player);
+	}
+	//
+
+	private void createConfig() {
 		configf = new File(getDataFolder() + File.separator + "config.yml");
 		config = YamlConfiguration.loadConfiguration(configf);
-		
+
 		boolean save = false;
 		if(!configf.exists()) {
 			List<String> defaultlist = new ArrayList<String>();
@@ -78,97 +142,137 @@ public class vlist extends JavaPlugin implements Listener{
 			}
 		}
 	}
-	
+
 	/*EventHandler for Player's joining the server*/
 	@EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-        Player p = e.getPlayer();
-        if ((p.hasPermission("vox.list.staff")) && (!staff.contains(p.getName()))) {
-            if (hasNickName(p)) {
-                   staff.add(this.chat.getPlayerPrefix(p) + "~" + ess.getUser(p).getNickname());
-            } else {
-        staff.add(this.chat.getPlayerPrefix(p) + p.getName());
-            }
-            }
+	private void onJoin(PlayerJoinEvent e) {
+		Player p = e.getPlayer();
+		if (p.hasPermission("vox.list.staff") && !staff.containsKey(p.getUniqueId())) {
+			String title = getPrefix(p) + (hasNickName(p) ? "~" + getNickname(p) : p.getName());
+			staff.put(p.getUniqueId(), title);
+		}
+
+
+		//		if ((p.hasPermission("vox.list.staff")) && (!staff.contains(p.getName()))) {
+		//			if (hasNickName(p)) {
+		//				staff.add(this.chat.getPlayerPrefix(p) + "~" + ess.getUser(p).getNickname());
+		//			} else {
+		//				staff.add(this.chat.getPlayerPrefix(p) + p.getName());
+		//			}
+		//		}
 	}
+
 	/*EventHandler for Player's leaving the server*/	
 	@EventHandler
-	public void onQuit(PlayerQuitEvent e) {
-	    Player p = e.getPlayer();
-        if (staff.contains(hasNickName(p))) {
-       	 staff.remove(this.chat.getPlayerPrefix(p) + ess.getUser(p).getNickname());
-        } else {
-       	 staff.remove(this.chat.getPlayerPrefix(p) + p.getName());
-        }
+	private void onQuit(PlayerQuitEvent e) {
+		Player player = e.getPlayer();
+		if (staff.containsKey(player.getUniqueId())) {
+			staff.remove(player.getUniqueId());
+		}
+		//NOTE: The if statement above is not really necessary.
+
+		//if (staff.contains(hasNickName(p))) {
+		//	staff.remove(this.chat.getPlayerPrefix(p) + ess.getUser(p).getNickname());
+		//} else {
+		//	staff.remove(this.chat.getPlayerPrefix(p) + p.getName());
+		//}
 	}
 	/*/list command*/
+	@Override //make it clear this method is from another class.
 	public boolean onCommand(CommandSender s, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("list")) {
 			if (s instanceof Player) {
-			for (String listMessage : getConfig().getStringList("ListMessage")) {
-				s.sendMessage(ChatColor.translateAlternateColorCodes('&', listMessage
-						.replace("{onlineplayers}", Integer.toString(Bukkit.getOnlinePlayers().size()))
-						.replace("{staff}", getStaffMessage())
-						.replace("{>>}", "»")));
-			}
-			}
-		}
-	return true;
-	}
-	/*Adding in the <name>, <name> feature (i.e. TheMasteredPanda, xExpandable, HoneyHax*/
-	public String getStaffMessage() {
-		if (staff.size() >= 1) {
-			return Joiner.on(ChatColor.YELLOW + ", ").join(staff);
-		}
-		return getConfig().getString("None");
-	}
-	
-	/*Implementation for getting the prefix (I think)*/
-	public boolean setupChat() {
-		RegisteredServiceProvider<Chat> chatProvider = getServer().getServicesManager().getRegistration(Chat.class);
-	if (chatProvider != null) {
-		this.chat = ((Chat)chatProvider.getProvider());
-	    }
-		return this.chat != null;
-	}
-	
-	@EventHandler
-	public boolean onPreCommand(PlayerCommandPreprocessEvent e) {
-		Player p = e.getPlayer();
-		if (p instanceof Player) {
-		if (e.getMessage().equals("v")) {
-			if (user.isHidden(p)) {
-				if (staff.contains(hasNickName(p))) {
-					staff.remove(this.chat.getPlayerPrefix(p) + "~" + ess.getUser(p).getNickname());
-				} else {
-					staff.remove(this.chat.getPlayerPrefix(p) + p.getName());
+				for (String listMessage : getConfig().getStringList("ListMessage")) {
+					s.sendMessage(ChatColor.translateAlternateColorCodes('&', listMessage
+							.replace("{onlineplayers}", Integer.toString(Bukkit.getOnlinePlayers().size()))
+							.replace("{staff}", getStaffMessage())
+							.replace("{>>}", "»")));
 				}
-				} else {
-					if (hasNickName(p)) {
-						staff.add(this.chat.getPlayerPrefix(p) + "~" + ess.getUser(p).getNickname());
-					} else {
-						staff.add(this.chat.getPlayerPrefix(p) + p.getName());
-					}
-				}
-		}
+			}
 		}
 		return true;
 	}
-	
-	public void checkList() {
-		for (Player p : Bukkit.getServer().getOnlinePlayers()) {
-	        if (staff.contains(hasNickName(p))) {
-	          	 staff.remove(this.chat.getPlayerPrefix(p) + ess.getUser(p).getNickname());
-	           } else {
-	          	 staff.remove(this.chat.getPlayerPrefix(p) + p.getName());
-	           }
-	        if ((p.hasPermission("vox.list.staff")) && (!staff.contains(p.getName()))) {
-	           if (hasNickName(p)) {
-	              staff.add(this.chat.getPlayerPrefix(p) + "~" + ess.getUser(p).getNickname());
-	           } else {
-	              staff.add(this.chat.getPlayerPrefix(p) + p.getName());
-	           }
-	        }      
+	/*Adding in the <name>, <name> feature (i.e. TheMasteredPanda, xExpandable, HoneyHax*/
+	private String getStaffMessage() {
+		if (staff.size() >= 1) {
+			//return Joiner.on(ChatColor.YELLOW + ", ").join(staff);
+			return Joiner.on(ChatColor.YELLOW + ", ").join(staff.values());
 		}
+		return getConfig().getString("None");
+	}
+
+	/*Implementation for getting the prefix (I think)*/
+	private boolean setupChat() {
+		RegisteredServiceProvider<Chat> chatProvider = getServer().getServicesManager().getRegistration(Chat.class);
+		if (chatProvider != null) {
+			this.chat = ((Chat)chatProvider.getProvider());
+		}
+		return this.chat != null;
+	}
+
+	@EventHandler
+	private boolean onPreCommand(PlayerCommandPreprocessEvent e) {
+		//NOTE: You also need to check for vanish here, and any other aliases
+		//Essentials might have for the command.
+		//Also use equalsIgnoreCase()
+		Player p = e.getPlayer();
+		User user = ess.getUser(p);
+		//		if (p instanceof Player) {
+		//if (e.getMessage().equals("v")) {
+		//NOTICE the addition of the /
+		if (e.getMessage().equalsIgnoreCase("/vanish") || e.getMessage().equalsIgnoreCase("/v")) {
+			if (!user.isHidden(p)) {//NOTICE the addition of ! if they're not hidden we will be hiding them
+				//if (staff.contains(hasNickName(p))) {
+				if (staff.containsKey(p.getUniqueId())) {
+					//staff.remove(this.chat.getPlayerPrefix(p) + "~" + ess.getUser(p).getNickname());
+					staff.remove(p.getUniqueId());
+				} //else {
+				//						staff.remove(this.chat.getPlayerPrefix(p) + p.getName());
+				//					}
+			} else {
+				//Note my use of the ternary operator here, comes in handy :P
+				String title = getPrefix(p) + (hasNickName(p) ? "~" + getNickname(p) : p.getName());
+				staff.put(p.getUniqueId(), title);
+				//					if (hasNickName(p)) {
+				//						staff.add(this.chat.getPlayerPrefix(p) + "~" + ess.getUser(p).getNickname());
+				//					} else {
+				//						staff.add(this.chat.getPlayerPrefix(p) + p.getName());
+				//					}
+			}
+		}
+		//		}
+		return true;
+	}
+
+	//I see you are not using this code currently, but it has some invalid logic that needs to be fixed.
+	private void checkList() {
+		for (Player p : Bukkit.getOnlinePlayers()) {
+			if (staff.containsKey(p.getUniqueId())) {
+				if (!p.hasPermission("vox.list.staff")) {
+					staff.remove(p.getUniqueId());
+				}
+			} else {
+				if (p.hasPermission("vox.list.staff")) {
+					String title = getPrefix(p) + (hasNickName(p) ? "~" + getNickname(p) : p.getName());
+					staff.put(p.getUniqueId(), title);
+				}
+			}
+		}
+
+
+		//		for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+		//			if (staff.contains(hasNickName(p))) {
+		//				staff.remove(this.chat.getPlayerPrefix(p) + ess.getUser(p).getNickname());
+		//			} else {
+		//				staff.remove(this.chat.getPlayerPrefix(p) + p.getName());
+		//			}
+		//			if ((p.hasPermission("vox.list.staff")) && (!staff.contains(p.getName()))) {
+		//				if (hasNickName(p)) {
+		//					staff.add(this.chat.getPlayerPrefix(p) + "~" + ess.getUser(p).getNickname());
+		//				} else {
+		//					staff.add(this.chat.getPlayerPrefix(p) + p.getName());
+		//				}
+		//			}      
+		//		}
 	}
 }
